5e701fcec53a50fdc717ffb70e902f1fb42a2259
ChrisBeirne <c.w.beirne@gmail.com>
2023-07-31 15:43:13 -0700

    Upload code

---
+++ 1_data_import_and_housekeeping.R
@@ -0,0 +1,307 @@
+# Final analysis for climate corridor paper
+
+# Create file folders
+dir.create("data")
+dir.create("data/input")
+dir.create("data/output")
+
+# Load packages
+
+list.of.packages <- c("stars",       
+                      "sf",
+                      "dplyr",            
+                      "leaflet",
+                      "googledrive",
+                      "purrr",
+                      "remotes",
+                      "reticulate",
+                      "MetBrewer",
+                      "terra",
+                      "raster",
+                      "gdistance",
+                      "nngeo",
+                      "stringr")
+
+# Check which ones you dont have
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+#Install the ones you dont have
+if(length(new.packages)) install.packages(new.packages)
+# Load the packages
+lapply(list.of.packages, require, character.only = TRUE)
+
+# Auth google drive
+drive_auth()
+
+# Import data files
+
+# Note - Derived files are created in the connectivity modelling script: 
+# https://github.com/ChrisBeirne/Osa-Conservation-Connectivity-Project
+
+# Specify aoi
+aoi <-  ext(-84, -82.5,8.38,9.38)
+
+#################################
+# Aoi folder
+gd_folder = "https://drive.google.com/drive/folders/1uLmpJVwMsv_OX5Tpd0eh3dfwJSz2aV1g"
+folder_id = drive_get(as_id(gd_folder))
+files = drive_ls(folder_id)
+
+    ## base aoi
+    tmp <- files[substr(files$name,1,8)=="base_aoi",]
+    # Download - note #fails if already exists
+       for (file_i in seq_along(tmp$name)) {
+          try({
+           drive_download(
+             as_id(tmp$id[file_i]),
+             path = paste0("data/input/", tmp$name[file_i])
+              )
+           })
+         }
+    
+    base_shp <- st_read("data/input/base_aoi.shp", quiet = TRUE)
+    
+    ## Focal countries
+    tmp <- files[substr(files$name,1,15)=="focal_countries",]
+    # Download - note #fails if already exists
+    for (file_i in seq_along(tmp$name)) {
+      try({
+        drive_download(
+          as_id(tmp$id[file_i]),
+          path = paste0("data/input/", tmp$name[file_i])
+        )
+      })
+    }
+    
+    focal_shp <- st_read("data/input/focal_countries.shp", quiet = TRUE)
+    # plot(focal_shp)
+    
+    
+    ## High elevation contiguous habitat
+    tmp <- files[substr(files$name,1,22)=="high_elevation_forests",]
+    # Download - note #fails if already exists
+    for (file_i in seq_along(tmp$name)) {
+      try({
+        drive_download(
+          as_id(tmp$id[file_i]),
+          path = paste0("data/input/", tmp$name[file_i])
+        )
+      })
+    }
+    
+    end_nodes <- st_read("data/input/high_elevation_forests_5km2_17N.shp", quiet = TRUE)
+
+    
+    ## Waterbody masl
+    ## High elevation contiguous habitat
+    tmp <- files[files$name=="waterbody_mask.tif",]
+    # Download - note #fails if already exists
+    for (file_i in seq_along(tmp$name)) {
+      try({
+        drive_download(
+          as_id(tmp$id[file_i]),
+          path = paste0("data/input/", tmp$name[file_i])
+        )
+      })
+    }
+    
+    mask <- raster("data/input/waterbody_mask.tif")
+    
+    # Contiguous start PA's
+    tmp <- files[substr(files$name,1,28)=="low_elevation_pas_contiguous",]
+    # Download - note #fails if already exists
+    for (file_i in seq_along(tmp$name)) {
+      try({
+        drive_download(
+          as_id(tmp$id[file_i]),
+          path = paste0("data/input/", tmp$name[file_i])
+        )
+      })
+    }
+    
+    start_pas_cont <- st_read("data/input/low_elevation_pas_contiguous.shp")
+
+########################################
+    # Individual protected areas
+    
+    gd_folder = "https://drive.google.com/drive/u/1/folders/1KgNuMrKPyskUmroOBGISt3FxAsaBLmRv"
+    folder_id = drive_get(as_id(gd_folder))
+    files = drive_ls(folder_id)
+    
+    ## base aoi
+    tmp <- files[substr(files$name,1,23)=="lowland_protected_areas",]
+    # Download - note #fails if already exists
+    for (file_i in seq_along(tmp$name)) {
+      try({
+        drive_download(
+          as_id(tmp$id[file_i]),
+          path = paste0("data/input/", tmp$name[file_i])
+        )
+      })
+    }
+    
+    start_pas <- st_read("data/input/lowland_protected_areas.shp", quiet = TRUE)
+    
+    start_pas$LOW_AREA_KM <- as.numeric(st_area(start_pas)/(1000*1000))
+    start_pas <- start_pas[start_pas$LOW_AREA_KM>=5,]
+    
+    # Clean the names
+
+    start_pas$NAME <- iconv(str_replace_all(start_pas$NAME, "[^[:alnum:]]", " "), from = 'UTF-8', to = 'ASCII//TRANSLIT')
+    
+    
+    
+    
+    
+########################################
+
+  ## land cover layer (100m)
+      # landcover folder
+      gd_folder = "https://drive.google.com/drive/u/1/folders/11asmjqIxnbno-EwW9Qe8CqzvZk1SofmX"
+      folder_id = drive_get(as_id(gd_folder))
+      files = drive_ls(folder_id)
+      
+      ## landcover
+      tmp <- files[substr(files$name,1,24)=="ESACCI-global-100m_merge",]
+      # Download - note #fails if already exists
+      for (file_i in seq_along(tmp$name)) {
+        try({
+          drive_download(
+            as_id(tmp$id[file_i]),
+            path = paste0("data/input/", tmp$name[file_i])
+          )
+        })
+      }
+      
+      # Labels
+      tmp <- files[files$name=="ESA_v2_labels.csv",]
+      # Download - note #fails if already exists
+      for (file_i in seq_along(tmp$name)) {
+        try({
+          drive_download(
+            as_id(tmp$id[file_i]),
+            path = paste0("data/input/", tmp$name[file_i])
+          )
+        })
+      }
+      
+      lc_100m <- rast("data/input/ESACCI-global-100m_merged.tif")
+      lc_key <- read.csv("data/input/ESA_v2_labels.csv", header=T, sep=",")
+
+###########################################
+
+## Biomass layer
+
+    # biomass folder
+    gd_folder = "https://drive.google.com/drive/u/1/folders/1LB-gpbCbb0c8E8xpCc_gEOd6CmL69Sd3"
+    folder_id = drive_get(as_id(gd_folder))
+    files = drive_ls(folder_id)
+    
+    ## base aoi
+    tmp <- files[files$name=="NASA_biomass_desnity_estimation.tif",]
+    # Download - note #fails if already exists
+    for (file_i in seq_along(tmp$name)) {
+      try({
+        drive_download(
+          as_id(tmp$id[file_i]),
+          path = paste0("data/input/", "NASA_biomass_desnity_estimation.tif")
+        )
+      })
+    }
+    
+    biomass <- rast("data/input/NASA_biomass_desnity_estimation.tif")
+    
+
+
+
+    
+    
+        
+########################################################################
+########################################################################
+########################################################################
+# Create the conductnace layer
+
+    # Format the files
+    base_terra <- vect(base_shp)
+    lc_key$rgb <- paste0("#",lc_key$rgb ) 
+    lc_100m <- mask(lc_100m, base_terra)
+    
+    # Set up reclassify table
+    m2     <- c(0, NA,
+                10, 1000,
+                20, 150,
+                30, 30,
+                40, 30,
+                50, NA,
+                60, 40,
+                70, NA,
+                80, 30,
+                90, 20,
+                95, 500,
+                100,2
+    )
+    m2 <- matrix(m2, ncol=2, byrow = TRUE)
+    
+    # Reclassify the full thing?
+    lc_re <- classify(lc_100m, m2, right=TRUE)
+    # Convert to raster and take a look
+    lc_re_ras <- raster(lc_re)
+    plot(lc_re_ras)
+    
+    # Create the biomass modifier
+    biomass <- clamp(biomass, upper=100)
+    #plot(biomass)
+    biomass_modifier <- biomass/100
+    plot(biomass_modifier)
+    
+    #We then mask out non-forest areas, and apply the modifier, which creates a surface which looks like:
+    just_forest <- ifel(lc_100m == 10, 1, NA)
+    
+    #plot(just_forest)
+    # Match the resolutions
+    biomass_modifier <-  resample(biomass_modifier, just_forest)
+    #plot(biomass_modifier)
+    #plot(just_forest,add=T)
+    
+    # Keep non-forest areas the same too!                      
+    final_modifier <- mask(biomass_modifier,just_forest)
+    # Keep none forest as they are -> 1 (as we ultimately take the prodict)
+    final_modifier <- subst(final_modifier, NA, 1)
+
+    
+    
+    # Implement the modifier
+    cost_final <- lc_re*final_modifier
+    
+    # Mask out large water bodies - use the archived conductance layer
+    mask[mask>0]<-1
+    #plot(cond_1km)
+    tmp <- rast(mask)
+    tmp <- as.polygons(tmp)
+    
+    tmp <- project(tmp,cost_final )
+    #plot(tmp)
+    cost_final  <-mask(cost_final, tmp)
+    plot(cost_final)
+    
+# Write this raster and upload it to google drive
+    
+writeRaster(cost_final, "Data/output/High_res_resistance_surface_final.tif", overwrite=T)
+    
+tmp <- project(cost_final, "+proj=utm +zone=17 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs")
+
+# Also make a version with NA's removed (required for least cost path models)
+tmp2 <- classify(tmp, cbind(NA, 0.000001))
+cost_final_17N <- as(tmp,"Raster")
+cost_final_17N_NO_NA <- as(tmp2,"Raster")
+
+writeRaster(cost_final_17N, "Data/output/High_res_resistance_surface_final_17N.tif", overwrite=T)
+writeRaster(cost_final_17N_NO_NA, "Data/output/High_res_resistance_surface_final_17N_NO_NA.tif", overwrite=T)
+
+# Try a 200m Res
+
+remove(files, final_modifier, biomass, biomass_modifier, folder_id, just_forest,
+       m2, mask, tmp, tmp2, lc_re_ras, lc_ras, lc_re)
+       
+
+
---
+++ 2_Least_cost_path_models.R
@@ -0,0 +1,208 @@
+# Run script 1_ before running this script
+
+# View start end end_nodes
+
+leaflet() %>%
+  addProviderTiles("OpenStreetMap", group="OSM (default)") %>% 
+  addProviderTiles(providers$OpenTopoMap, group="Topo") %>%
+  addProviderTiles(providers$Esri.WorldImagery, group="Satellite") %>%  # Add satellite data
+  addPolygons(data=start_pas, weight = 2, fillColor = "red", label=start_pas$NAME, stroke=F, fillOpacity=1) %>%
+  addPolygons(data=st_transform(end_nodes, 4326), weight = 2, fillColor = "blue", label=, stroke=F, fillOpacity=1) %>%
+  #addPolygons(data=tmp, weight = 2, fillColor = "blue") %>%
+  # Layers control
+  addLayersControl(
+    baseGroups = c("OSM (default)","Satellite", "Topo"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+
+###################################################
+# First define the end nodes
+
+# Give each one an ID code
+end_nodes$NODE_CODE <- paste0("H",sprintf("%04d", 1:nrow(end_nodes)))
+
+# Convert to 17N
+end_nodes_wgs <- st_transform(end_nodes, 4326)
+plot(st_geometry(end_nodes), border=F, col="red")
+
+
+# Centroids of all contiguous forest fragments
+end_points <- sf::st_point_on_surface(end_nodes)
+
+# Try adding in some other locations (to account for large polygons)
+set.seed(1200)
+test <- st_sample(end_nodes, 250, type = "hexagonal")
+
+# Intersect to get the information back
+test <- st_intersection(end_nodes, test)
+
+end_points <- rbind(end_points, test)
+# Give them an endpoint code
+end_points$END_CODE <- paste0("END",sprintf("%04d", 1:nrow(end_points)))
+
+end_points_wgs<- st_transform(end_points, 4326)
+# I need to remove any that are too close
+#st_nn(end_points)
+
+leaflet() %>%
+  addProviderTiles("OpenStreetMap", group="OSM (default)") %>%
+  addProviderTiles("Esri.WorldImagery", group="Satellite") %>%
+  #addRasterImage(tmp, group="Raster") %>% 
+  addPolygons(data=end_nodes_wgs, fillColor = "yellow",
+              group="End_nodes") %>% 
+  addCircleMarkers(data=end_points_wgs, col="black", radius=1) %>% 
+  addLayersControl(
+    baseGroups = c("OSM (default)", "Satellite"),
+    overlayGroups = c("End_nodes"))
+
+# Convert start PA's to 17N
+start_pas_wgs <- start_pas
+start_pas <- st_transform(start_pas, 31971)
+
+dir.create("data/output/plots")
+
+# Run the loop
+# Make a list to store the files
+res_terrestrial <- list()
+res_shp <- list()
+for(i in 1:nrow(start_pas)) #in 401:485)#
+{
+    A <- as_Spatial(st_point_on_surface(start_pas[i,]))
+    counter <- i#-400
+    # Find nearest neighbours - in this case 3
+    # If you need it to run fast us k =1
+    near_locs <- st_nn(start_pas[i,], end_points, k=5)
+    near_locs <- unlist(near_locs)
+    tmp_shp <- list()
+    tmp_frame <- data.frame("ID"=rep(start_pas$NAME[i], length(near_locs)), "endID"=NA, "length_km"=NA, "conductance"=NA)
+    # Create the transition matrix by subsetting the conductance surface
+    t1 <- st_as_sf(A)
+    t2 <- end_points[near_locs,]
+    t1 <- rbind(st_coordinates(t1),st_coordinates(t2))
+    t1 <- t1 %>%
+    as.data.frame %>%
+    sf::st_as_sf(coords = c(1,2))
+    #plot(t1)
+    # BUFFER
+    t2 <- st_buffer(t1, 50000)
+    # CROP RASTER
+    tmp_ras <- crop(cost_final_17N_NO_NA, st_bbox(t2))
+    # Create transition
+    cond_derived <- transition(tmp_ras,function(x) mean(x), 8)
+    for(j in 1:length(near_locs))     #nrow())
+    {
+    B <- as_Spatial(end_points[near_locs[j],])
+    tmp_frame$endID[j] <- B$NODE_CODE
+    # Erase the previous
+    lc_path <-NULL
+    #plot(cond_1km_utm)
+    #plot(A, add=T)
+    #plot(B, add=T)
+    # I have conductance so I need : function(x) 1/mean(x)
+    # Get the shortest path
+    lc_path <- gdistance::shortestPath(cond_derived, origin=A,
+    goal=B, output="SpatialLines")
+    #plot(lc_path, add=T)
+    # Need a break if this cannot run
+    tmp <- st_as_sf(lc_path)
+    if(nrow(st_coordinates(tmp$geometry))>2)
+    {
+    #plot(st_geometry(tmp))
+    #plot(cond_1km, add=T)
+    #plot(st_geometry(tmp), add=T, lwd=2)
+    #plot(st_geometry(st_centroid(terrestrial_17N[i,])), add=T, pch=19)
+    #plot(st_geometry(st_centroid(high_pa_17N[near_locs[j],])), add=T, pch=19, col="red")
+    # Distance in km
+    tmp_frame$length_km[j] <- round(as.numeric(st_length(tmp))/1000,1)
+    # Condusctance units
+    tmp3 <- raster::extract(cost_final_17N, tmp)
+    tmp_frame$conductance[j] <- sum(tmp3[[1]], na.rm=T)
+    # Mean conductancer
+    tmp_frame$cond_min[j] <- min(tmp3[[1]], na.rm=T)
+    tmp_frame$cond_mean[j] <- mean(tmp3[[1]], na.rm=T)
+    tmp$ID <- start_pas$NAME[i]
+    tmp$endID <- tmp_frame$endID[j]
+    tmp_shp[[j]] <- tmp
+    print(paste("terrestrial", i, "- loop", j))
+    }
+    }
+    res_terrestrial[[counter]] <- tmp_frame
+    res_shp[[counter]] <- tmp_shp
+    png(filename=paste0("data/output/plots/",tmp_frame$ID[1], ".jpg"), width = 800, height = 800)
+    plot(tmp_ras, main=tmp_frame$ID[1])
+    plot(st_geometry(bind_rows(tmp_shp)), add=T)
+    plot(st_geometry(t1), add=T,pch=c(19,2,2,2,2,2) )
+    dev.off()
+}
+
+res_terrestrial_df <- bind_rows(res_terrestrial)
+res_terrestrial_shp <- bind_rows(res_shp)
+
+# Note I actually split this loop into five to run them in parallel.
+
+saveRDS(res_terrestrial_df, "tmp/res_ter_401_485.RDS")
+saveRDS(res_terrestrial_shp, "tmp/res_ter_shp_401_485.RDS")
+
+# Read in results dataframe and shapefiles
+
+## Dataframes
+df1 <- readRDS("tmp/res_ter_1_100.RDS")
+df2 <- readRDS("tmp/res_ter_101_200.RDS")
+df3 <- readRDS("tmp/res_ter_201_300.RDS")
+df4 <- readRDS("tmp/res_ter_301_400.RDS")
+df5 <- readRDS("tmp/res_ter_401_485.RDS")
+
+res_df <- bind_rows(df1, df2, df3,df4,df5)
+
+## Shapefiles
+shp1 <- readRDS("tmp/res_ter_shp_1_100.RDS")
+shp2 <- readRDS("tmp/res_ter_shp_101_200.RDS")
+shp3 <- readRDS("tmp/res_ter_shp_201_300.RDS")
+shp4 <- readRDS("tmp/res_ter_shp_301_400.RDS")
+shp5 <- readRDS("tmp/res_ter_shp_401_485.RDS")
+
+res_shp <- bind_rows(shp1, shp2, shp3,shp4,shp5)
+#plot(st_geometry(res_shp))
+
+# Import the corrtions (where the simulation didnt work due to landscape shape)
+update_df <- readRDS("tmp/res_ter_problems.RDS")
+update_shp <- readRDS("tmp/res_ter_shp_problems.RDS")
+
+# Remove the ones with issues from the dataframe
+
+res_df <- res_df[!(res_df$ID %in% update_df$ID),]
+res_shp <- res_shp[!(res_shp$ID %in% update_shp$ID),]
+
+# Add in the corrections
+final_df <- bind_rows(res_df, update_df)
+final_shp <- bind_rows(res_shp, update_shp)
+
+# Clean up
+remove(df1,df2,df3,df4,df5,shp1,shp2,shp3,shp4,shp5, update_df, update_shp, res_df, res_shp)
+
+# Write key files
+dir.create("data/output/corridors")
+
+# Data 
+st_write(final_shp, "data/output/corridors/all_corridors.shp")
+write.csv(final_df, "data/output/corridors/all_corridors_df.csv")
+
+# Also save the protected areas and end nodes
+dir.create("data/output/nodes")
+
+# Sort out label consistency
+colnames(end_nodes)[colnames(end_nodes)=="NODE_CODE"] <- "endID"
+end_nodes$area <- round(end_nodes$area,1)
+end_nodes$are_km2 <- round(end_nodes$are_km2,1)
+
+colnames(end_points)[colnames(end_points)=="NODE_CODE"]<- "endID"
+end_points$are_km2 <- round(end_points$are_km2,1)
+end_points$area <- round(end_points$area,1)
+
+sf::st_write(start_pas, "data/output/nodes/start_nodes.shp")
+st_write(end_nodes, "data/output/nodes/end_nodes.shp", append=F)
+st_write(end_points, "data/output/nodes/end_points.shp", append=F)
+
+
+
+
